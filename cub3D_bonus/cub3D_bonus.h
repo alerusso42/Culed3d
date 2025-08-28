0119690de08e2fe891021322e222555547e6dd52
Alessandro Russo <alerusso@c1r4p6.42firenze.it>
2025-08-28 12:33:40 +0200

    aggiunto un .h

--- cub3D_bonus/cub3D_bonus.h
+++ cub3D_bonus/cub3D_bonus.h
@@ -6,7 +6,7 @@
 /*   By: alerusso <alerusso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 17:19:17 by alerusso          #+#    #+#             */
-/*   Updated: 2025/08/28 09:18:59 by alerusso         ###   ########.fr       */
+/*   Updated: 2025/08/28 12:33:05 by alerusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@
 # include <stdlib.h>
 # include <X11/X.h>
 # include <X11/keysym.h>
-# include "libft/libft.h"	
+# include "libft/libft.h"
+# include "files_bonus.h"
 
 # define RST		"\033[0m" /* Reset to default color */
 # define RED		"\033[1;31m" /* Bold Red */
@@ -49,69 +50,6 @@
 # define PLAYER_CHARS "NSEWP"
 # define FFILL_CHARS "0NSEW"
 # define WALL_CHARS "1 "
-
-# define SCREEN_TXTR "textures/screen.xpm"
-# define PLAYER_TXTR "textures/pisnelo.xpm"
-# define WALL_TXTR "textures/mini_wall.xpm"
-# define DOOR_OPEN_TXTR "textures/door_open.xpm"
-# define DOOR_CLOSE_TXTR "textures/door_closed.xpm"
-# define CROSS_TXTR "textures/Crosshair.xpm"
-# define BAGUETTE_TXTR "textures/BAGUETTE.xpm"
-# define ARMS1_TXTR "textures/possenza.xpm"
-# define ARMS2_TXTR "textures/possenza2.xpm"
-# define COIN_TXTR "textures/coin.xpm"
-# define FOE1_TXTR "textures/foe_1.xpm"
-# define FOE2_TXTR "textures/foe_2.xpm"
-# define FOE3_TXTR "textures/foe_3.xpm"
-# define FOE4_TXTR "textures/foe_4.xpm"
-# define FOE5_TXTR "textures/foe_5.xpm"
-# define FOE6_TXTR "textures/foe_6.xpm"
-# define FOE7_TXTR "textures/foe_7.xpm"
-# define FOE8_TXTR "textures/foe_8.xpm"
-# define FOE9_TXTR "textures/foe_9.xpm"
-# define FOE10_TXTR "textures/foe_10.xpm"
-# define FOE11_TXTR "textures/foe_11.xpm"
-# define FOE12_TXTR "textures/foe_12.xpm"
-# define FOE_BIT1_TXTR "textures/barbarian_1left.xpm"
-# define FOE_BIT2_TXTR "textures/barbarian_1left.xpm"
-# define FOE_BIT3_TXTR "textures/barbarian_1right.xpm"
-# define FOE_BIT4_TXTR "textures/barbarian_2right.xpm"
-# define MINI_COIN_TXTR "textures/mini_coin.xpm"
-# define MINI_FOE_TXTR "textures/mini_foe.xpm"
-# define MINI_BACKGROUND_TXTR "textures/mini_background.xpm"
-# define MINI_PLAYER_0_TXTR "textures/mini_player_0.xpm"
-# define MINI_PLAYER_20_TXTR "textures/mini_player_20.xpm"
-# define MINI_PLAYER_40_TXTR "textures/mini_player_40.xpm"
-# define MINI_PLAYER_60_TXTR "textures/mini_player_60.xpm"
-# define MINI_PLAYER_80_TXTR "textures/mini_player_80.xpm"
-# define MINI_PLAYER_100_TXTR "textures/mini_player_100.xpm"
-# define MINI_PLAYER_120_TXTR "textures/mini_player_120.xpm"
-# define MINI_PLAYER_140_TXTR "textures/mini_player_140.xpm"
-# define MINI_PLAYER_160_TXTR "textures/mini_player_160.xpm"
-# define MINI_PLAYER_180_TXTR "textures/mini_player_180.xpm"
-# define MINI_PLAYER_200_TXTR "textures/mini_player_200.xpm"
-# define MINI_PLAYER_220_TXTR "textures/mini_player_220.xpm"
-# define MINI_PLAYER_240_TXTR "textures/mini_player_240.xpm"
-# define MINI_PLAYER_260_TXTR "textures/mini_player_260.xpm"
-# define MINI_PLAYER_280_TXTR "textures/mini_player_280.xpm"
-# define MINI_PLAYER_300_TXTR "textures/mini_player_300.xpm"
-# define MINI_PLAYER_320_TXTR "textures/mini_player_320.xpm"
-# define MINI_PLAYER_340_TXTR "textures/mini_player_340.xpm"
-# define MINI_PLAYER_360_TXTR "textures/mini_player_360.xpm"
-# define MINI_DOOR_TXTR "textures/mini_door.xpm"
-# define MENU_FRAME_TXTR "textures/menu_il_destino.xpm"
-# define MENU_PLAY_TXTR "textures/play_button.xpm"
-# define MENU_EXIT_TXTR "textures/exit_button.xpm"
-
-# if PC_ALE == true
-#  define SFX_OP "/home/alerusso/Culed3d/cub3D_bonus/sfxs/main_menu.wav"
-#  define SFX_GAME "/home/alerusso/Culed3d/cub3D_bonus/sfxs/game.wav"
-#  define SFX_ENEMY "/home/alerusso/Culed3d/cub3D_bonus/sfxs/enemy.wav"
-# else
-#  define SFX_OP "/home/alerusso/Desktop/Culed3d/cub3D_bonus/sfxs/main_menu.wav"
-#  define SFX_GAME "/home/alerusso/Desktop/Culed3d/cub3D_bonus/sfxs/game.wav"
-#  define SFX_ENEMY "/home/alerusso/Desktop/Culed3d/cub3D_bonus/sfxs/enemy.wav"
-# endif
 
 # define HIMG 64
 # define WIMG 64
