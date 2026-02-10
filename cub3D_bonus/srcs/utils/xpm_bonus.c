bbd825773e55c89a048641b0566fb910400ece30
alerusso <alessandro.russo.frc@gmail.com>
2026-02-10 01:30:49 +0100

    push

--- cub3D_bonus/srcs/utils/xpm_bonus.c
+++ cub3D_bonus/srcs/utils/xpm_bonus.c
@@ -6,7 +6,7 @@
 /*   By: alerusso <alessandro.russo.frc@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 12:07:11 by alerusso          #+#    #+#             */
-/*   Updated: 2026/02/10 01:23:25 by alerusso         ###   ########.fr       */
+/*   Updated: 2026/02/10 01:28:31 by alerusso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ void	parse_xpm(t_data *data, t_txtr *txtr, int name)
 	int	i;
 	int	colors;
 	int	chars_per_color;
+	t_map	map;
 
-	i = 10;
-	if (!line)
-		return (error(data, E_MLX_TEXTURE, name));
-	
+	i = 10;	
 	txtr->size[X] = ft_atoi(&g_textures[name][i]) * 4;
 	i += sub_strlen(&g_textures[name][i], " ", EXCLUDE);
 	txtr->size[Y] = ft_atoi(&g_textures[name][i]);
@@ -32,6 +30,8 @@ void	parse_xpm(t_data *data, t_txtr *txtr, int name)
 	i += sub_strlen(&g_textures[name][i], " ", EXCLUDE);
 	chars_per_color = ft_atoi(&g_textures[name][i]);
 	txtr->total_size = txtr->size[X] * txtr->size[Y];
-	free(line);
-	gnl_statik(fd, true);
+	while (colors)
+	{
+
+	}
 }
